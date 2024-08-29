ad24a9a0d225864c472620a13cff58faf28755ba
nunu <maxime.clog@pm.me>
2024-08-29 19:08:08 +0200

    Consolidation du script d'installation

--- Init.ps1
+++ Init.ps1
@@ -1,14 +1,21 @@
 ﻿<# Script d'initialisation du module Powershell #>
 
+# S'assure de la cohérence du répertoire module/
 if (Test-Path -Path .\module) {
     Remove-Item -Path .\module -Recurse -Force
 }
-
 New-Item -Name module -ItemType Directory
 
+# S'assure de l'existance du répertoire datas/
 if (-not (Test-Path -Path .\datas)){
     New-Item -Name .\datas -ItemType Directory
 }
 
+# Télécharge le module à jour
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/module/pf2ToScribeModule.psm1" -OutFile .\module\pf2ToScribeModule.psm1
+
+# Import du module
 Import-Module -Name .\module\pf2ToScribeModule.psm1
+
+# Prêt !
+Write-Host "pf2-to-scribe prêt !"
--- Run.ps1
+++ Run.ps1
@@ -1,2 +1,7 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/Init.ps1" -OutFile .\Init.ps1
+# Télécharge le script d'initialisation à jour
+if (-not (Test-Path -Path .\Init.ps1) -or ((Get-Item -Path .\Init.ps1).LastWriteTime -lt (Get-Date).AddMinutes(-1))){
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/Init.ps1" -OutFile .\Init.ps1 -ErrorAction Stop
+}
+
+# Lancement de la fenêtre d'exécution
 powershell.exe -NoExit -Command "./Init.ps1"
