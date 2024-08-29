f31dd77cd8f65144bff109680646942d7016b8f4
nunu <maxime.clog@pm.me>
2024-08-29 19:31:47 +0200

    Fix typo sur l'écriture de la progression et encodage

--- Run.ps1
+++ Run.ps1
@@ -1,10 +1,10 @@
-$ProgressPreference = 'SilentlyContinue'
+﻿$ProgressPreference = 'SilentlyContinue'
 
 # Télécharge le script d'initialisation à jour
 if (-not (Test-Path -Path .\Init.ps1) -or ((Get-Item -Path .\Init.ps1).LastWriteTime -lt (Get-Date).AddMinutes(-1))){
     Write-Host "Mise à jour du script d'initialisation..." -NoNewline
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/Init.ps1" -OutFile .\Init.ps1 -ErrorAction Stop
-    Write-Host "Mise à jour du script d'initialisation... OK"
+    Write-Host "OK" -ForegroundColor Green
 }
 
 # Lancement de la fenêtre d'exécution
