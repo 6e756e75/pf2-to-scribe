a6ff9bbc15974ed04986c55c15a4773882c6b0a9
nunu <maxime.clog@pm.me>
2024-08-29 19:20:32 +0200

    Amélioration diverses sur le script d'initialisation

--- Init.ps1
+++ Init.ps1
@@ -1,21 +1,36 @@
 ﻿<# Script d'initialisation du module Powershell #>
 
+Write-Host "Installation du module..."
 # S'assure de la cohérence du répertoire module/
 if (Test-Path -Path .\module) {
     Remove-Item -Path .\module -Recurse -Force
 }
-New-Item -Name module -ItemType Directory
-
-# S'assure de l'existance du répertoire datas/
-if (-not (Test-Path -Path .\datas)){
-    New-Item -Name .\datas -ItemType Directory
-}
+New-Item -Name module -ItemType Directory | Out-Null
 
 # Télécharge le module à jour
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/module/pf2ToScribeModule.psm1" -OutFile .\module\pf2ToScribeModule.psm1
 
 # Import du module
 Import-Module -Name .\module\pf2ToScribeModule.psm1
+Write-Host "Installation du module... OK"
+
+Write-Host "Installation du répertoire des données..."
+# S'assure de l'existance du répertoire datas/
+if (-not (Test-Path -Path .\datas)){
+    New-Item -Name .\datas -ItemType Directory | Out-Null
+}
+Write-Host "Installation du répertoire des données... OK"
 
 # Prêt !
-Write-Host "pf2-to-scribe prêt !"
+Write-Host `
+"  _____  ______ ___    _           _____           _ _          
+ |  __ \|  ____|__ \  | |         / ____|         (_) |         
+ | |__) | |__     ) | | |_ ___   | (___   ___ _ __ _| |__   ___ 
+ |  ___/|  __|   / /  | __/ _ \   \___ \ / __| '__| | '_ \ / _ \
+ | |    | |     / /_  | || (_) |  ____) | (__| |  | | |_) |  __/
+ |_|    |_|    |____|  \__\___/  |_____/ \___|_|  |_|_.__/ \___|
+----------------------------------------------------------------                                                                
+                                                      v2024.08.1
+
+cf. https://github.com/6e756e75/pf2-to-scribe pour l'aide.
+"
