066ba0b2f2b03dc39c97325833e291fd66f277ad
nunu <maxime.clog@pm.me>
2024-08-31 17:22:43 +0200

    Mise à jour de la version du script d'initialisation

--- Run.ps1
+++ Run.ps1
@@ -2,7 +2,7 @@
 
 # Télécharge le script d'initialisation à jour
 if (-not (Test-Path -Path .\Init.ps1) -or ((Get-Item -Path .\Init.ps1).LastWriteTime -lt (Get-Date).AddMinutes(-1))){
-    Write-Host "Mise à jour du script d'initialisation..." -NoNewline
+    Write-Host "Mise à jour du script d'initialisation... " -NoNewline
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/Init.ps1" -OutFile .\Init.ps1 -ErrorAction Stop
     Write-Host "OK" -ForegroundColor Green
 }
