836391fa46901b474e41c563a9e96d1f9d8d494e
nunu <maxime.clog@pm.me>
2024-09-03 20:00:43 +0200

    Améliore l'initialisation et le lancement du module

--- Run.ps1
+++ Run.ps1
@@ -1,11 +1,2 @@
-﻿$ProgressPreference = 'SilentlyContinue'
-
-# Télécharge le script d'initialisation à jour
-if (-not (Test-Path -Path .\Init.ps1) -or ((Get-Item -Path .\Init.ps1).LastWriteTime -lt (Get-Date).AddMinutes(-1))){
-    Write-Host "Mise à jour du script d'initialisation... " -NoNewline
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/Init.ps1" -OutFile .\Init.ps1 -ErrorAction Stop
-    Write-Host "OK" -ForegroundColor Green
-}
-
-# Lancement de la fenêtre d'exécution
-powershell.exe -NoExit -Command "./Init.ps1"
+﻿# Lancement de la fenêtre d'exécution
+powershell.exe -NoExit -Command "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/6e756e75/pf2-to-scribe/main/Init.ps1'))"
